cc5e42e663f2a566de6e395ed2f2e175fc13f7aa
LoneAndroid <gayduk.matwey@gmail.com>
2017-03-08 18:55:25 +0300

    Тайная Канцелярия
    Исправление политических ошибок Yotara II  Тайной Канцеляриейпривело к
    упадку лояльности народа к нловому Князю...

--- app/src/main/java/topsecretinformation/javaproject_v11/StartActivity.java
+++ app/src/main/java/topsecretinformation/javaproject_v11/StartActivity.java
@@ -1,7 +1,15 @@
 package topsecretinformation.javaproject_v11;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) { // если нажали на
+            case android.R.id.home: // кнопку перехода в чат
+                DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawwwwer_layout);
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                return true;
+            default:
+                return true;
+        }
+    }
+
 }
